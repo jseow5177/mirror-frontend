c4efe83896c013fed4447ba85fb775a30aea94d6
jonathan.seow <jseow5177@gmail.com>
2025-03-09 12:11:02 +0800

    Fix dynamic

--- src/app/dashboard/settings/roles/page.tsx
+++ src/app/dashboard/settings/roles/page.tsx
@@ -2,6 +2,8 @@ import { getActions, getRoles } from '@/app/_lib/data/role';
 import { getMe } from '@/app/_lib/data/users';
 import Roles from '@/app/_ui/settings/roles';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Page() {
   const fetchRolesData = async () => {
     const [actions, roles, me] = await Promise.all([
